e0ed5b286085cbdd3ba4bf84f1d62cdfc9eb6937
Taylan Aydinli <taylanaydinli@gmail.com>
2013-09-12 16:32:22 +0300

    Implemented s3 and imgur integration
    The script retrieves all single imgur links. It also
    gets all images in all galleries using the imgur api.
    It disregards all other image sources and other links.
    
    I also implemented the upload to s3 feature.
    
    The code is still somewhat messy. I'm just trying to
    get it to work. I'll clean it up later

--- crawler.py
+++ crawler.py
@@ -1,14 +1,61 @@
 #!/usr/bin/env python
 
 from urllib2 import urlopen, HTTPError, URLError
+import pyimgur
 import re
 import os
+from boto.s3.connection import S3Connection
+from pyimgur import Imgur, Gallery_album
+from boto.s3.key import Key
+import yaml
+import time
+
 import shutil
 
 from bs4 import BeautifulSoup
 
+
+def get_album_images(url):
+    album_id = os.path.splitext(os.path.basename(url))[0]
+    im = Imgur(os.environ['IMGURCLIENTID'])
+    album = im.get_album(album_id)
+    album_images = [image.link for image in album.images]
+
+    return album_images
+
+
+def initialize_config():
+    if [k for k in ['S3KEY', 'S3SECRET', 'IMGURCLIENTID', 'IMGURSECRET'] if k in os.environ.keys()]:
+        print('all keys exist')
+        return
+
+    if not os.path.exists('env.yaml'):
+        raise Exception('env.yaml required for s3 parameter initialization')
+
+    with open('env.yaml', 'r') as config_file:
+        config = yaml.load(config_file)
+        os.environ['S3KEY'] = config['s3config']['key']
+        os.environ['S3SECRET'] = config['s3config']['secret']
+        os.environ['IMGURCLIENTID'] = config['imgurconfig']['clientid']
+        os.environ['IMGURSECRET'] = config['imgurconfig']['secret']
+
+
+def upload_file(bucket, url):
+    print "uploading main image ", os.path.basename(url)
+    main_img_key = Key(bucket)
+    main_img_key.key = os.path.basename(url)
+    main_img_key.set_contents_from_filename(os.path.join(files_dir, os.path.basename(url)), reduced_redundancy=True)
+    main_img_key.make_public()
+
+    thumb_img_key = Key(bucket)
+    thumb_img_key.key = os.path.basename(re.sub(image_extension_regex, r't.\1', url))
+    thumb_img_key.set_contents_from_filename(os.path.join(files_dir, os.path.basename(re.sub(image_extension_regex, r't.\1', url))), reduced_redundancy=True)
+    thumb_img_key.make_public()
+
+
 def download_file(url):
     try:
+        print('downloading ' + url)
         thumb_url = re.sub(image_extension_regex, r't.\1', url)
 
         image_source = urlopen(url)
@@ -28,34 +75,78 @@ def download_file(url):
         print "HTTP Error:", e.code, url
     except URLError, e:
         print "URL Error:", e.reason, url
+    except:
+        print 'awesome error ', url
 
+initialize_config()
+print os.environ['S3KEY']
+print os.environ['S3SECRET']
 
 files_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "files")
 if not os.path.exists(files_dir):
     os.mkdir(files_dir)
 
-imgur_single_image_regex = re.compile("^https?:\/\/(?:i\.|m\.|edge\.|www\.)*imgur\.com\/(?!gallery)(?!removalrequest)(?!random)(?!memegen)([A-Za-z0-9]{5}|[A-Za-z0-9]{7})[sbtmlh]?(\.(?:jpe?g|gif|png))?$", re.IGNORECASE)
-#imgur_image_regex = re.compile("^https?:\/\/(?:i\.|m\.|edge\.|www\.)*imgur\.com\/(?!gallery)(?!removalrequest)(?!random)(?!memegen)(a/)?([A-Za-z0-9]{5}|[A-Za-z0-9]{7})[sbtmlh]?(\.(?:jpe?g|gif|png))?(\?.*)?$", IGNORECASE)
+#imgur_single_image_regex = re.compile("^https?:\/\/(?:i\.|m\.|edge\.|www\.)*imgur\.com\/(?!gallery)(?!removalrequest)(?!random)(?!memegen)([A-Za-z0-9]{5}|[A-Za-z0-9]{7})[sbtmlh]?(\.(?:jpe?g|gif|png))?$", re.IGNORECASE)
+imgur_image_regex = re.compile("^(https?:\/\/(?:i\.|m\.|edge\.|www\.)*imgur\.com\/(?!gallery)(?!removalrequest)(?!random)(?!memegen)(a/)?([A-Za-z0-9]{5}|[A-Za-z0-9]{7})[sbtmlh]?(\.(?:jpe?g|gif|png))?)(\?.*)?$", re.IGNORECASE)
 image_extension_regex = re.compile("\.(jpe?g|gif|png)$", re.IGNORECASE)
+gallery_url_regex = re.compile("/a/", re.IGNORECASE)
 
-#page = urllib2.urlopen('http://www.reddit.com/r/aww')
-file = open('aww.txt', 'r')
-soup = BeautifulSoup(file.read())
-#file.write(soup.prettify().encode('utf-8'))
-#file.flush()
-#file.close()
+html = None
+if not os.path.exists('aww.txt') or os.stat('aww.txt').st_size == 0:
+    print 'aww.txt does not exist or is empty. going to reddit'
+    page = urlopen('http://www.reddit.com/r/aww?limit=100', timeout=20)
+    html = page.read()
+    with open('aww.txt', 'w') as file:
+        try:
+            file.write(html)
+        except UnicodeDecodeError, e:
+            print "UnicodeDecodeError: ", e.reason
+else:
+    print "opening cached file"
+    html = open('aww.txt', 'r').read()
+
+soup = BeautifulSoup(html)
 entries = soup.select("div.entry a.title")
+links = []
+for entry in entries:
+    mtc = imgur_image_regex.match(entry['href'])
+    if not mtc:
+        continue
+    links.append(mtc.group(1))
 
-links = [entry['href'] for entry in entries if imgur_single_image_regex.match(entry['href'])]
+images = []
 
+print("collecting gallery images")
+gallery_collect_start = time.time()
 for i, link in enumerate(links):
-    if not image_extension_regex.search(link):
-        links[i] = link + '.jpg'
+    if gallery_url_regex.search(link):
+        [images.append(img) for img in get_album_images(link)]
+    elif not image_extension_regex.search(link):
+        images.append(link + '.jpg')
+    else:
+        images.append(link)
+gallery_collect_end = time.time()
 
 print(links)
+print(images)
 
-for l in links[0:2]:
-    download_file(l)
+print "got {0} links and {1} images in {2} seconds".format(len(links), len(images), gallery_collect_end - gallery_collect_start)
+
+download_start_timestamp = time.time()
+for img in images:
+    download_file(img)
+download_end_timestamp = time.time()
+
+print "downloaded {0} images in {1} seconds".format(len(images), download_end_timestamp - download_start_timestamp)
+
+
+#s3conn = S3Connection(os.environ['S3KEY'], os.environ['S3SECRET'])
+#bucket = s3conn.create_bucket('ta-subreddit-mirror-images')
+#bucket.set_acl('public-read')
+#
+#for l in links[0:1]:
+#    upload_file(bucket, l)
+
 
 #shutil.rmtree(files_dir)
 
